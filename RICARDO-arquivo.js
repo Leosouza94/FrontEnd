3fe03df25b3a078d26e197fe1d88db9ca7729f48
Leosouza94 <leosouza94@hotmail.com>
2023-09-29 14:22:57 -0300

    Add files via upload

---
+++ RICARDO-arquivo.js
@@ -0,0 +1,24 @@
+
+/*
+// Captura o botão pelo ID
+const meuBotao = document.getElementById("meuBotao");
+
+
+// Função para ser executada quando o botão for clicado
+function redirecioneParaMinhaPagina() {
+    // Redireciona o usuário para minha página
+    window.location.href = "https://www.bol.uol.com.br/entretenimento/2022/12/05/copa-do-mundo-meme-2014.htm"
+};
+
+
+// Define o evento de clique no botão
+meuBotao.addEventListener("click", redirecioneParaMinhaPagina());
+*/
+
+function redirecionarParaOutroSite() {
+    // URL do site para onde você deseja redirecionar
+    var url = "https://www.bol.uol.com.br/entretenimento/2022/12/05/copa-do-mundo-meme-2014.htm";
+
+    // Redirecionar para a nova URL
+    window.location.href = url;
+}
